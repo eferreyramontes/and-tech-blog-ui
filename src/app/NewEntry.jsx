f11e918f5bca12995894240de99a9b9d4f52d869
Emmanuel <eferreyramontes@gmail.com>
2020-04-14 14:36:53 +0100

    Use Yup to create validations

--- src/app/NewEntry.jsx
+++ src/app/NewEntry.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Formik, Form } from 'formik';
 import PageContent from './../components/PageContent';
-import FormField from './../components/FormField'
+import FormField from './../components/FormField';
+import * as Yup from "yup";
 
 class NewEntry extends React.Component {
     render() {
+        const options = ["Java", "React", "Python"];
+
+        const schemaValidation = Yup.object().shape({
+            title: Yup.string()
+                .min(2, 'Too Short!')
+                .max(70, 'Too Long!')
+                .required('Required'),
+            tech: Yup.string().oneOf(options),
+            content: Yup.string()
+                .min(2, 'Too Short!')
+                .required('Required'),
+        });
+
         return (
             <PageContent>
                 <Formik
                     initialValues={{ title: '', tech: '', status: '', content: '', tags: [] }}
-                    validate={values => {
-                        const errors = {};
-                        if (!values.title) {
-                            errors.title = 'Required';
-                        }
-                        if (!values.tech) {
-                            errors.tech = 'Select one of the available techs';
-                        }
-                        if (!values.content) {
-                            errors.content = 'Required';
-                        }
-                        return errors;
-                    }}
+                    validationSchema={schemaValidation}
                     onSubmit={(values, { setSubmitting }) => {
                         setTimeout(() => {
                             alert(JSON.stringify(values, null, 2));
@@ -35,7 +37,7 @@ class NewEntry extends React.Component {
                         return (
                             <Form className="form">
                                 <FormField name="title" title="Title" type="text" placeholder="Article title" />
-                                <FormField name="tech" title="Tech" type="select" placeholder="Article tech" options={["Java", "React", "Python"]} />
+                                <FormField name="tech" title="Tech" type="select" placeholder="Article tech" options={options} />
                                 <FormField name="status" title="Status" type="text" placeholder="Article status" />
                                 <FormField name="content" title="Content" type="textarea" placeholder="Article content" />
                                 <FormField name="tags" title="Tags" type="text" placeholder="Article tags" />
